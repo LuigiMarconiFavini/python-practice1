69a7bbee5bf9145efc65d2f0659a74d16686d7f1
LuigiMarconiFavini <luigimarconifavini@gmail.com>
2023-09-06 11:15:24 -0300

    Ejercicio 7 Práctica 1

--- exercises/exercise7.py
+++ exercises/exercise7.py
@@ -9,7 +9,8 @@ y en el mismo orden.
 lista = ["casa", "perro", "pato", "gato"]
 
 # COMPLETAR - INICIO
-
+# la funcion tuple convierte la lista en una tupla
+tupla = tuple(lista)
 # COMPLETAR - FIN
 
 assert tupla == ("casa", "perro", "pato", "gato")
@@ -23,7 +24,8 @@ y en el mismo orden.
 tupla = "casa", "perro", "pato", "gato", "tenedor"
 
 # COMPLETAR - INICIO
-
+# la funcion list convierte una tupla en una lista
+lista = list(tupla)
 # COMPLETAR - FIN
 
 assert lista == ["casa", "perro", "pato", "gato", "tenedor"]
@@ -36,7 +38,7 @@ Desempaquetar la siguiente tupla en las variables a, b y c
 tupla = ("primer", 25, [1, 2, 3])
 
 # COMPLETAR - INICIO
-
+a, b, c = tupla
 # COMPLETAR - FIN
 
 assert a == "primer" and b == 25 and c == [1, 2, 3]
@@ -49,7 +51,8 @@ Desempaquetar la siguiente tupla y luego sumar sus valores
 tupla = (87, 98, 35, 67, 4, 9)
 
 # COMPLETAR - INICIO
-
+a, b, c, d, e, f =tupla
+total= a+b+c+d+e+f
 # COMPLETAR - FIN
 
 assert total == 300
@@ -63,7 +66,9 @@ Restricción: Utilizar f-Strings.
 lista = ["esta", "mañana", "sali", "a", "correr"]
 
 # COMPLETAR - INICIO
-
+a, b, c, d, e = lista
+string_concatenado = f"{a} {b} {c} {d} {e}"
+# si dejas espacios entre las llaves te deja un espacio entre las palabras
 # COMPLETAR - FIN
 
 assert string_concatenado == "esta mañana sali a correr"
@@ -77,7 +82,8 @@ Restricción: Utilizar desempaquetado con comodines
 tupla = (73, 45, 344, 3434, 2)
 
 # COMPLETAR - INICIO
-
+primer, *_ = tupla
+# primer toma el primer valor de la tupla y el resto lo guardo en la variable guión bajo con el comodín *
 # COMPLETAR - FIN
 
 assert primer == 73
@@ -91,7 +97,8 @@ Restricción: Utilizar desempaquetado con comodines
 lista = [73, 45, 344, 3434, 2]
 
 # COMPLETAR - INICIO
-
+primer, *_, ultimo = lista
+suma = primer+ultimo
 # COMPLETAR - FIN
 
 assert suma == 75
@@ -106,7 +113,8 @@ Restricción: Utilizar desempaquetado con comodines y f-Strings
 tupla = ("anoche", "fui", "a", "la", "fiesta", "pero", "no", "pude", "entrar")
 
 # COMPLETAR - INICIO
-
+primer, segundo, tercer, cuarto, quinto, *_ = tupla
+string_concatenado = f"{primer} {segundo} {tercer} {cuarto} {quinto}"
 # COMPLETAR - FIN
 
 assert string_concatenado == "anoche fui a la fiesta"
